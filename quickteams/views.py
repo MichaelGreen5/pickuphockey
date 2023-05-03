5040091f015e2f51fbcacf6eb4a80d0463463aeb
Michael Green <mikegreenrecording@gmail.com>
2023-05-03 19:22:41 -0400

    quickteams update

--- quickteams/views.py
+++ quickteams/views.py
@@ -33,19 +33,20 @@ class DeleteQuickPlayer(DeleteView):
 
  
 def QuickTeams(request):
-    
+    #TODO prevent pop from empty list, reset button
     from OrgDash.team_sort import SortTeams
     active_user = request.user
     my_players = QuickPlayer.objects.filter(created_by=active_user)
     skaters = []
     goalies = []
+    
     for player in my_players:
         if player.goalie & player.here:
             goalies.append(player)
         elif player.here:
             skaters.append(player)
-
-
+    num_players_here = len(skaters) + len(goalies)
+    
     #Take player skill from player objs
     player_data =[((player), float(player.skill)) for player in skaters]
     goalie_data = [((player), float(player.skill)) for player in goalies]
@@ -73,26 +74,30 @@ def QuickTeams(request):
         quick_bench_obj.save()
     
    
-   
+    
     total_skill = (float(quick_dark_obj.skill) + float(quick_light_obj.skill))
     
     context = {'light_team': quick_light_obj.team.all(), 'dark_team': quick_dark_obj.team.all(), 'total_skill':total_skill, 'my_players':my_players,
-                'light_team_skill' : quick_light_obj.skill, 'dark_team_skill': quick_dark_obj.skill, 'bench_members': quick_bench_obj.bench_members.all()}
+                'light_team_skill' : quick_light_obj.skill, 'dark_team_skill': quick_dark_obj.skill, 'bench_members': quick_bench_obj.bench_members.all(),
+                'num_players_here': num_players_here}
     if request.method == 'POST':
         here_action = request.POST.get('here_form_action')
         sort_action = request.POST.get('auto_sort')
+        clear_action = request.POST.get('clear_teams')
         
         if here_action == 'confirm_here':
             whos_here = request.POST.getlist('here_player')
+            
             for player in my_players:
                 if str(player.pk) in whos_here:
                     player.here = True
+                    
                 else:
                     player.here = False
                 player.save()
             
 
-        
+       
         if sort_action == 'auto_sort':
             teams = SortTeams(player_data, goalie_data)
             
@@ -103,6 +108,10 @@ def QuickTeams(request):
             # set dark team
             dark_team_members = [player[0] for player in teams[1]]
             quick_dark_obj.team.set(dark_team_members)
+
+        if clear_action == 'clear_teams':
+            quick_light_obj.team.clear()
+            quick_dark_obj.team.clear()
 
         return redirect(reverse('quickteams:quick_teams' ),context)
     else:
